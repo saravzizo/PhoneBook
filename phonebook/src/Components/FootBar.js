596b9ca19e96fbad4699659bfcb356136740a790
saravzizo <saravanan472002@gmail.com>
2024-03-20 13:25:13 +0530

    footbar complete coloring and icons

--- phonebook/src/Components/FootBar.js
+++ phonebook/src/Components/FootBar.js
@@ -1,19 +1,54 @@
-const FootBar =() =>{
+
+import React, { useState } from 'react'
+const FootBar = () => {
+
+    const [isClicked, setIsClicked] = useState([false, false, false]);
+
+    const handleClick = (index) => {
+        const newIsClicked = [false, false, false];
+        newIsClicked[index] = true;
+        setIsClicked(newIsClicked);
+    };
+
+
     return (
-        <div className="h-10 w-full flex items-center justify-center p-8 border-t border-gray-300 border-opacity-20 " style={{position:"sticky", bottom:"0",backgroundColor:"black"}}>
-            
-            <div className="flex flex-col items-center justify-center mx-5 ">
-                <i className="bi bi-star"></i>
-                <p className="text-xs">Favorites</p>
+        <div className="h-10 w-full flex items-center justify-center p-8 border-t border-gray-300 border-opacity-20 " style={{ position: "sticky", bottom: "0", backgroundColor: "black" }}>
+
+            <div className="flex flex-col items-center justify-center mx-5 " onClick={() => handleClick(0)}>
+                {isClicked[0] ? <div className='text-blue-500 text-center'>
+                    <i class="bi bi-person-fill"></i>
+                    <p className="text-xs">Contacts</p>
+                </div>
+                    : <div className='text-center'>
+                        <i class="bi bi-person"></i>
+                        <p className="text-xs">Contacts</p>
+                    </div>
+                }
             </div>
-            <div className="flex flex-col items-center justify-center mx-5 ">
-                <i class="bi bi-moon-stars"></i>
-                <p className="text-xs">Theme</p>
+
+            <div className="flex flex-col items-center justify-center mx-5 " onClick={() => handleClick(1)}>
+                {isClicked[1] ? <div className='text-blue-500 text-center'>
+                    <i class="bi bi-star-fill"></i>
+                    <p className="text-xs">Favourates</p>
+                </div>
+                    : <div className='text-center'>
+                        <i class="bi bi-star"></i>
+                        <p className="text-xs">Favourates</p>
+                    </div>
+                }
+
             </div>
-           
-           
-            
-            
+            <div className="flex flex-col items-center justify-center mx-5 " onClick={() => handleClick(2)}>
+                {isClicked[2] ? <div className='text-blue-500 text-center'>
+                    <i class="bi bi-moon-stars-fill"></i>
+                    <p className="text-xs">Theme</p>
+                </div>
+                    : <div className='text-center'>
+                        <i class="bi bi-moon-stars"></i>
+                        <p className="text-xs">Theme</p>
+                    </div>
+                }
+            </div>
 
         </div>
     )
