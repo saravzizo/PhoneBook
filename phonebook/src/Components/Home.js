f68043ec4de8e129d118faa56566e74120cac06f
saravzizo <saravanan472002@gmail.com>
2024-03-21 14:29:08 +0530

    select all contact done, nned to hit api

--- phonebook/src/Components/Home.js
+++ phonebook/src/Components/Home.js
@@ -47,12 +47,35 @@ const Home = () => {
 
     }
 
-
     const [isEdit, setIsEdit] = useState(false);
 
     const handleEdit = () =>{
+        setTimeout(() => {
         setIsEdit(true);
+        },100);
     }
+
+    const [checkedContacts, setCheckedContacts] = useState([]);
+
+    const handleCheck = (id) => {
+        setCheckedContacts(prevState => {
+            if (prevState.includes(id)) {
+                return prevState.filter(contactId => contactId !== id);
+            } else {
+                return [...prevState, id];
+            }
+        });
+    };
+
+    const handleSelectAll = () => {
+        if (checkedContacts.length === res.length) {
+            setCheckedContacts([]);
+        } else {
+            setCheckedContacts(res.map((m,index ) => index));
+        }
+    };
+
+    console.log(checkedContacts)
 
 
     return (
@@ -66,11 +89,15 @@ const Home = () => {
                         <div className="container bg-black text-white xs:w-1/2 sm:w-2/3 lg:w-1/3 h-screen " >
                             <div className="sticky">
                                 <div className="flex items-center px-6 mb-6 justify-center pt-8 pb-3">
-                                    <p className="text-2xl font-semibold">Contacts</p>
+
+                                    {isEdit ? <p className="text-lg font-semibold" onClick={handleSelectAll}>Select All</p> : <p className="text-2xl font-semibold">Contacts</p>}
+                                    
                                     <span className="grow "></span>
-                                    <p className="text-xl font-semibold pl-5 cursor-pointer" onClick={handleEdit}>Edit</p>
-                                    <p className="text-3xl px-5 cursor-pointer"><i className="bi bi-plus" onClick={handlePlus}></i></p>
-                                    <p className="text-xl cursor-pointer"><i className="bi bi-gear" onClick={handleSettings}></i></p>
+                                    {isEdit ? <p className="text-lg font-semibold cursor-pointer" onClick={() => setIsEdit(false)}>Cancel</p> : null}
+
+                                    {!isEdit && <p className="text-xl font-semibold pl-5 cursor-pointer" onClick={handleEdit}>Edit</p>}
+                                    {!isEdit && <p className="text-3xl px-5 cursor-pointer"><i className="bi bi-plus" onClick={handlePlus}></i></p>}
+                                    {!isEdit && <p className="text-xl cursor-pointer"><i className="bi bi-gear" onClick={handleSettings}></i></p>}
                                 </div>
 
                                 <Search />
@@ -88,8 +115,8 @@ const Home = () => {
                                 </div>
                             </div>
 
-                            <ContactList isEdit= {isEdit} res= {res} />
-                            <FootBar />
+                            <ContactList isEdit= {isEdit} res= {res}  handleCheck= {handleCheck}  checkedContacts={checkedContacts}/>
+                            <FootBar  isEdit= {isEdit}/>
                         </div>
 
                        
